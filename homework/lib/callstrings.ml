b18fa889c9ff209adb5d722ba237a982812423e2
Ned Williamson <edwillia@andrew.cmu.edu>
2015-03-12 08:27:02 -0400

    track address so we can print later

--- homework/lib/callstrings.ml
+++ homework/lib/callstrings.ml
@@ -12,7 +12,7 @@ let calls syms insns =
             | None -> calls
             | Some (mem, dst) ->
               if Addr.(Memory.min_addr mem = addr) then
-                Seq.cons dst calls
+                Seq.cons (addr, dst) calls
               else calls
           else calls
       end) in visitor#run (Insn.bil insn) Seq.empty)
@@ -21,8 +21,9 @@ let all_calls p =
   let string_tab = Table.mapi p.symbols ~f:(
     fun mem src ->
       Seq.map (calls p.symbols (Disasm.insns_at_mem p.program mem)) ~f:(
-        fun dst -> (src, dst))) in
+        fun (addr, dst) -> (addr, src, dst))) in
   let flattened = Seq.concat_map ~f:snd (Table.to_sequence string_tab) in
-  Seq.mapi flattened ~f:(fun i (src,dst) -> (i, src, dst))
+  (* we might want to fold into a table here from i to the tuple *)
+  Seq.mapi flattened ~f:(fun i (addr, src, dst) -> (i, addr, src, dst))
 
 let callstrings p root = Seq.empty
